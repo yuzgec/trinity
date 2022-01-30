37aad2937a3455cf81d7435ca87ef29b12eafe1a
yuzgec <olcayy@gmail.com>
2022-01-31 00:13:58 +0300

    Fix

--- app/Http/Requests/ServiceRequest.php
+++ app/Http/Requests/ServiceRequest.php
@@ -15,7 +15,7 @@ class ServiceRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'                 => 'required|min:6|max:99|unique:service,title,'.$this->id,
+            'title'                 => 'required|min:3|max:99|unique:service,title,'.$this->id,
             'category'              => 'required',
             'image'                 => 'image|max:2048|mimes:jpg,jpeg,png,gif',
             'gallery.*'             => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
@@ -27,7 +27,7 @@ class ServiceRequest extends FormRequest
         return [
             'title.required'            => 'Hizmet başlığını giriniz',
             'title.max'                 => 'Hizmet başlığı en fazla 99 karakter olabilir',
-            'title.min'                 => 'Hizmet başlığı en fazla 6 karakter olabilir',
+            'title.min'                 => 'Hizmet başlığı en az 3 karakter olabilir',
             'title.unique'              => 'Hizmet başlığı daha önce eklenmiş',
             'category.required'         => 'Hizmet Kategori seçimi zorunludur.',
             'image.max'                 => 'Resim boyutu en yüksek 2048kb(2mb) olmalıdır',
