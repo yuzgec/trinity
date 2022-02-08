327567dd06859a8b7c0c2102263c81832852b432
yuzgec <olcayy@gmail.com>
2022-02-08 05:50:47 +0300

    Fix

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Kalnoy\Nestedset\NodeTrait;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class ProductCategory extends Model
+class ProductCategory extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes, InteractsWithMedia, LogsActivity, NodeTrait;
+
+    protected $guarded = [];
+    protected $table = 'product_categories';
+
+    function getCategoryCount()
+    {
+        return $this->hasMany('App\Models\Product', 'category')->count();
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()->logOnly(['title', 'slug']);
+
+    }
+
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(400)
+            ->nonOptimized();
+    }
 }
