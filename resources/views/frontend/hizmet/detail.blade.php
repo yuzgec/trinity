0921f07843f1f34546aa066c50d23b9b883207c8
yuzgec <olcayy@gmail.com>
2022-02-21 00:56:19 +0300

    Fix

--- resources/views/frontend/hizmet/detail.blade.php
+++ resources/views/frontend/hizmet/detail.blade.php
@@ -32,7 +32,7 @@
                             <div class="col-md-6 col-lg-4 col-xl-3 {{ $row->getCategory->slug }}">
                                 <div class="top_courses">
                                     <div class="thumb">
-                                        <img class="img-whp" src="images/courses/t1.jpg" alt="t1.jpg">
+                                        <img class="img-whp" src="{{ (!$row->getFirstMediaUrl('page')) ? '/frontend/resimyok.jpg': $row->getFirstMediaUrl('page', 'thumb')}}" alt="{{ $row->title }}">
                                         <div class="overlay">
                                             <a class="tc_preview_course" href="{{ route('sinav.detay', [$row->getCategory->slug,$row->slug]) }}">İncele</a>
                                         </div>
