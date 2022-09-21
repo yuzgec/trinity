e02c84a42371cab7e8ec634b62bdf11b82ed8df2
yuzgec <olcayy@gmail.com>
2022-09-21 15:41:25 +0300

    Fix

--- resources/views/frontend/sss/index.blade.php
+++ resources/views/frontend/sss/index.blade.php
@@ -2,6 +2,21 @@
 @section('title')
 @section('content')
     @include('frontend.layout.header2')
-    @include('frontend.layout.pageheader')
+
+    <section class="inner_page_breadcrumb style2 blog_grid_bg_color" style="background-image: url('/frontend/images/headerback.png')">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 text-center">
+                    <div class="breadcrumb_content">
+                        <h1 class="text-white">Sıkça Sorulan Sorular</h1>
+                        <ol class="d-flex align-items-center justify-content-center">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}" class="text-white">Anasayfa</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Sıkça Sorulan Sorular</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
