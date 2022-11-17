4acefc051c5184941bde4c8894ff78c154450c1f
yuzgec <olcayy@gmail.com>
2022-11-17 10:29:06 +0300

    Fix

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>@yield('title', 'Trinity London College Türkiye Yetkili Sınav Merkezi')</title>
+    <title>@yield('title', 'Trinity College London Türkiye Yetkili Sınav Merkezi')</title>
     @include('frontend.layout.css')
 
 </head>
